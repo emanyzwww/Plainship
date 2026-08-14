f9fd4eee0182a813f9d7813d2bd568ebd982e5c9
Emanyzwww <emanyzwww@gmail.com>
2026-08-15 04:26:53 +0800

    release: v0.1.0 (重建)

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,7 +7,7 @@ package version
 // 发布构建通过 -ldflags "-X github.com/emanyzwww/plainship/internal/version.Version=<tag>" 注入;
 var Version = "0.1.0"
 
-// RendererVersion 返回渲染器版本 (跟随产品版本).
+// RendererVersion 返回渲染器版本.
 // 渲染输出随版本演进, 发版时强制全量重建, 保证发布产物与当前二进制一致.
 func RendererVersion() string {
 	return Version
